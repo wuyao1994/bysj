92c7b3d6bc3a30375b8110cf3aa00206a2f102e5
wuyao1994 <18380448929@163.com>
2016-05-10 23:25:59 +0800

    sql

--- sql/bysj.sql
+++ sql/bysj.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : wuyao
-Source Server Version : 50617
+Source Server Version : 50624
 Source Host           : localhost:3306
 Source Database       : bysj
 
 Target Server Type    : MYSQL
-Target Server Version : 50617
+Target Server Version : 50624
 File Encoding         : 65001
 
-Date: 2016-04-01 17:04:41
+Date: 2016-05-10 23:25:25
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -31,7 +31,7 @@ CREATE TABLE `admin_menu_info` (
   `sequence` int(2) DEFAULT NULL,
   `modelid` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`no`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of admin_menu_info
@@ -42,7 +42,8 @@ INSERT INTO `admin_menu_info` VALUES ('3', '系统用户', '/admin/system/viewAd
 INSERT INTO `admin_menu_info` VALUES ('4', '监测服务', null, '1', null, '2016-02-26 14:03:00', null, '1', '1', 'service');
 INSERT INTO `admin_menu_info` VALUES ('5', '系统菜单', '/admin/system/viewAdminMenus', '1', '1', '2016-02-26 14:03:32', null, '2', '3', 'system');
 INSERT INTO `admin_menu_info` VALUES ('6', '系统角色', '/admin/system/viewAdminRoles', '1', '1', '2016-02-26 14:04:40', null, '2', '4', 'system');
-INSERT INTO `admin_menu_info` VALUES ('7', '节点监测', '/zigbee/viewSensorMonitor', '1', '4', '2016-03-03 10:42:16', '2016-03-10 13:36:55', '2', '1', 'service');
+INSERT INTO `admin_menu_info` VALUES ('7', '节点管理手动模式', '/zigbee/viewSensorMonitor', '1', '4', '2016-03-03 10:42:16', '2016-04-18 10:53:44', '2', '1', 'service');
+INSERT INTO `admin_menu_info` VALUES ('8', '节点管理自动模式', '/zigbee/viewSensorMonitorAuto', '1', '4', '2016-04-18 10:53:27', '2016-04-18 10:53:27', '2', '2', 'service');
 
 -- ----------------------------
 -- Table structure for admin_role_info
@@ -88,10 +89,12 @@ INSERT INTO `admin_role_menu_grant` VALUES ('1', '4', '1', null, null, null);
 INSERT INTO `admin_role_menu_grant` VALUES ('1', '5', '1', null, null, null);
 INSERT INTO `admin_role_menu_grant` VALUES ('1', '6', '1', null, null, null);
 INSERT INTO `admin_role_menu_grant` VALUES ('1', '7', '1', null, null, null);
+INSERT INTO `admin_role_menu_grant` VALUES ('1', '8', '1', null, null, null);
 INSERT INTO `admin_role_menu_grant` VALUES ('2', '1', '1', null, null, null);
 INSERT INTO `admin_role_menu_grant` VALUES ('2', '2', '1', null, null, null);
 INSERT INTO `admin_role_menu_grant` VALUES ('2', '4', '1', null, null, null);
 INSERT INTO `admin_role_menu_grant` VALUES ('2', '7', '1', null, null, null);
+INSERT INTO `admin_role_menu_grant` VALUES ('2', '8', '1', null, null, null);
 
 -- ----------------------------
 -- Table structure for admin_user_info
@@ -142,22 +145,23 @@ CREATE TABLE `sensor_data_info` (
   `temperature_data` double DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
+  `name2` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of sensor_data_info
 -- ----------------------------
-INSERT INTO `sensor_data_info` VALUES ('1', 'led1', '0', '25.3', '33.33', '2016-04-01 10:21:49', '2016-04-01 16:27:11');
-INSERT INTO `sensor_data_info` VALUES ('2', 'led2', '1', '24.3', '22.22', '2016-04-01 10:21:52', '2016-04-01 16:19:46');
-INSERT INTO `sensor_data_info` VALUES ('3', 'led3', '1', '26.3', '23.22', '2016-04-01 10:21:54', '2016-04-01 16:19:47');
-INSERT INTO `sensor_data_info` VALUES ('4', 'led4', '0', '24.3', '24.2', '2016-04-01 10:21:57', '2016-04-01 14:46:25');
+INSERT INTO `sensor_data_info` VALUES ('1', 'led1', '0', '25', '29', '2016-04-01 10:21:49', '2016-05-10 23:23:45', '二层1号房间');
+INSERT INTO `sensor_data_info` VALUES ('2', 'led2', '0', '24', '23', '2016-04-01 10:21:52', '2016-05-09 22:30:24', '二层2号房间');
+INSERT INTO `sensor_data_info` VALUES ('3', 'led3', '0', '26.3', '23.22', '2016-04-01 10:21:54', '2016-04-16 13:45:10', '一层1号房间');
+INSERT INTO `sensor_data_info` VALUES ('4', 'led4', '1', '24.3', '24.2', '2016-04-01 10:21:57', '2016-04-18 11:17:45', '一层2号房间');
 
 -- ----------------------------
 -- View structure for admin_role_menu_grant_view
 -- ----------------------------
 DROP VIEW IF EXISTS `admin_role_menu_grant_view`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost`  VIEW `admin_role_menu_grant_view` AS select `admin_role_menu_grant`.`role_no` AS `role_no`,`admin_role_menu_grant`.`menu_no` AS `menu_no`,`admin_role_menu_grant`.`create` AS `create`,`admin_role_menu_grant`.`delete` AS `delete`,`admin_role_menu_grant`.`update` AS `update`,`admin_role_menu_grant`.`view` AS `view`,1 AS `active` from `admin_role_menu_grant` ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost`  VIEW `admin_role_menu_grant_view` AS select `admin_role_menu_grant`.`role_no` AS `role_no`,`admin_role_menu_grant`.`menu_no` AS `menu_no`,`admin_role_menu_grant`.`create` AS `create`,`admin_role_menu_grant`.`delete` AS `delete`,`admin_role_menu_grant`.`update` AS `update`,`admin_role_menu_grant`.`view` AS `view`,1 AS `active` from `admin_role_menu_grant` ; ;
 DROP TRIGGER IF EXISTS `admin_menu_info_BEFORE_DELETE`;
 DELIMITER ;;
 CREATE TRIGGER `admin_menu_info_BEFORE_DELETE` BEFORE DELETE ON `admin_menu_info` FOR EACH ROW delete from admin_role_menu_grant where menu_no = old.no
